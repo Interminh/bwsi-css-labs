7b101b3580f8829bb1266eb3204f458f945d7923
Minh Do <dotheminh2009@hotmail.com>
2026-06-23 13:02:27 -0700

    Added code to test branching

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -42,8 +42,20 @@ def main():
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    while True:
+        try:
+            num1 = float(input("Enter the first number: "))
+            return num1
+        except ValueError:
+            print("Invalid")
+
+    while True:
+        try:
+            num2 = float(input("Enter the first number: "))
+            return num2
+        except ValueError:
+            print("Invalid")
+
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
